55033d692aac48c8438b2e78ae2d73202a88d94c
strikeitbroke <strikeitbroke@gmail.com>
2024-10-11 20:08:55 -0700

    added admin

--- quickstart/admin.py
+++ quickstart/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from quickstart.models import Category, Author, Article, Comment
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Author)
+admin.site.register(Article)
+admin.site.register(Comment)
+
