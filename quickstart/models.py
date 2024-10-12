459ba0eae2babd73bceea29e184b3ea023b08b91
strikeitbroke <strikeitbroke@gmail.com>
2024-10-12 12:50:44 -0700

    moved the model up

--- quickstart/models.py
+++ quickstart/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 # Author model (Many Authors can write Many Articles)
 class Author(models.Model):
     class Meta:
         db_table = "author"
     name = models.CharField(max_length=100)
     bio = models.TextField(blank=True)
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
 
 
 # Article model (Many-to-Many with Authors, One-to-Many with Comments)
