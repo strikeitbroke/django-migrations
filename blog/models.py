b57e43a0ae23947561025bcf592a1fd708d61cfc
strikeitbroke <strikeitbroke@gmail.com>
2024-10-12 12:17:43 -0700

    updated the name back to article

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # article model (many-to-many with authors, one-to-many with comments)
 class Article(models.Model):
     class Meta:
-        db_table = "blog_article"
+        db_table = "article"
     title = models.CharField(max_length=200)
     content = models.TextField()
     authors = models.ManyToManyField('quickstart.Author', related_name='authors_to_%(app_label)s_%(class)s')  # Many-to-Many with Author
